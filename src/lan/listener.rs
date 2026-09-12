85ee2b6bb539a576fd1ecf8c5d7f7d84fb29bbb8
VocabVictor <83048106+VocabVictor@users.noreply.github.com>
2026-09-13 06:08:31 +0800

    refactor(app): move the discovery listener and peer query out of lan mod.rs
    Moved start_listening, discover, send_wol, get_broadcast_port, get_mac -> listener.rs; get_mac_by_ip, get_ipaddr_by_peer, create_broadcast_sockets, send_query, wait_response, spawn_wait_responses, handle_received_peers -> query.rs. pub(super) items became pub(in crate); private fns pub(super).

---
+++ src/lan/listener.rs
@@ -0,0 +1,98 @@
+use super::*;
+
+#[cfg(not(target_os = "ios"))]
+pub(in crate) fn start_listening() -> ResultType<()> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], get_broadcast_port()));
+    let socket = std::net::UdpSocket::bind(addr)?;
+    socket.set_read_timeout(Some(std::time::Duration::from_millis(1000)))?;
+    log::info!("lan discovery listener started");
+    loop {
+        let mut buf = [0; 2048];
+        if let Ok((len, addr)) = socket.recv_from(&mut buf) {
+            if let Ok(msg_in) = Message::parse_from_bytes(&buf[0..len]) {
+                match msg_in.union {
+                    Some(rendezvous_message::Union::PeerDiscovery(p)) => {
+                        if p.cmd == "ping"
+                            && config::option2bool(
+                                "enable-lan-discovery",
+                                &Config::get_option("enable-lan-discovery"),
+                            )
+                        {
+                            let id = Config::get_id();
+                            if p.id == id {
+                                continue;
+                            }
+                            if let Some(self_addr) = get_ipaddr_by_peer(&addr) {
+                                let mut msg_out = Message::new();
+                                let mut hostname = crate::whoami_hostname();
+                                // The default hostname is "localhost" which is a bit confusing
+                                if hostname == "localhost" {
+                                    hostname = "unknown".to_owned();
+                                }
+                                let peer = PeerDiscovery {
+                                    cmd: "pong".to_owned(),
+                                    mac: get_mac(&self_addr),
+                                    id,
+                                    hostname,
+                                    username: crate::platform::get_active_username(),
+                                    platform: whoami::platform().to_string(),
+                                    ..Default::default()
+                                };
+                                msg_out.set_peer_discovery(peer);
+                                socket.send_to(&msg_out.write_to_bytes()?, addr).ok();
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+pub async fn discover() -> ResultType<()> {
+    let sockets = send_query()?;
+    let rx = spawn_wait_responses(sockets);
+    handle_received_peers(rx).await?;
+
+    log::info!("discover ping done");
+    Ok(())
+}
+
+pub fn send_wol(id: String) {
+    let interfaces = default_net::get_interfaces();
+    for peer in &config::LanPeers::load().peers {
+        if peer.id == id {
+            for (_, mac) in peer.ip_mac.iter() {
+                if let Ok(mac_addr) = mac.parse() {
+                    for interface in &interfaces {
+                        for ipv4 in &interface.ipv4 {
+                            // remove below mask check to avoid unexpected bug
+                            // if (u32::from(ipv4.addr) & u32::from(ipv4.netmask)) == (u32::from(peer_ip) & u32::from(ipv4.netmask))
+                            log::info!("Send wol to {mac_addr} of {}", ipv4.addr);
+                            allow_err!(wol::send_wol(mac_addr, None, Some(IpAddr::V4(ipv4.addr))));
+                        }
+                    }
+                }
+            }
+            break;
+        }
+    }
+}
+
+#[inline]
+pub(super) fn get_broadcast_port() -> u16 {
+    (RENDEZVOUS_PORT + 3) as _
+}
+
+pub(super) fn get_mac(_ip: &IpAddr) -> String {
+    #[cfg(not(target_os = "ios"))]
+    if let Ok(mac) = get_mac_by_ip(_ip) {
+        mac.to_string()
+    } else {
+        "".to_owned()
+    }
+    #[cfg(target_os = "ios")]
+    "".to_owned()
+}
