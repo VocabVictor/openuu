f56c2e2405a71c2da919dd6b3392bcd8b76ed5ec
VocabVictor <83048106+VocabVictor@users.noreply.github.com>
2026-09-13 03:33:45 +0800

    refactor(common): move the pointer mouse regions out of remote_input.dart
    Final step of the remote_input split. New part pointer_regions.dart:
    RawPointerMouseRegion, CameraRawPointerMouseRegion. remote_input.dart keeps
    the imports, part list, RawKeyFocusScope, the special-hold-drag helpers,
    RawTouchGestureDetectorRegion and its State body. Every file in the
    directory is under 300 lines. No renames.
    flutter analyze: 245 issues / 0 errors (baseline).

---
+++ flutter/lib/common/widgets/remote_input/pointer_regions.dart
@@ -0,0 +1,92 @@
+part of 'remote_input.dart';
+
+class RawPointerMouseRegion extends StatelessWidget {
+  final InputModel inputModel;
+  final Widget child;
+  final MouseCursor? cursor;
+  final PointerEnterEventListener? onEnter;
+  final PointerExitEventListener? onExit;
+  final PointerDownEventListener? onPointerDown;
+  final PointerUpEventListener? onPointerUp;
+
+  RawPointerMouseRegion({
+    this.onEnter,
+    this.onExit,
+    this.cursor,
+    this.onPointerDown,
+    this.onPointerUp,
+    required this.inputModel,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerHover: inputModel.onPointHoverImage,
+      onPointerDown: (evt) {
+        onPointerDown?.call(evt);
+        inputModel.onPointDownImage(evt);
+      },
+      onPointerUp: (evt) {
+        onPointerUp?.call(evt);
+        inputModel.onPointUpImage(evt);
+      },
+      onPointerMove: inputModel.onPointMoveImage,
+      onPointerSignal: inputModel.onPointerSignalImage,
+      onPointerPanZoomStart: inputModel.onPointerPanZoomStart,
+      onPointerPanZoomUpdate: inputModel.onPointerPanZoomUpdate,
+      onPointerPanZoomEnd: inputModel.onPointerPanZoomEnd,
+      child: MouseRegion(
+        cursor: inputModel.isViewOnly
+            ? MouseCursor.defer
+            : (cursor ?? MouseCursor.defer),
+        onEnter: onEnter,
+        onExit: onExit,
+        child: child,
+      ),
+    );
+  }
+}
+
+class CameraRawPointerMouseRegion extends StatelessWidget {
+  final InputModel inputModel;
+  final Widget child;
+  final PointerEnterEventListener? onEnter;
+  final PointerExitEventListener? onExit;
+  final PointerDownEventListener? onPointerDown;
+  final PointerUpEventListener? onPointerUp;
+
+  CameraRawPointerMouseRegion({
+    this.onEnter,
+    this.onExit,
+    this.onPointerDown,
+    this.onPointerUp,
+    required this.inputModel,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerHover: (evt) {
+        final offset = evt.position;
+        double x = offset.dx;
+        double y = max(0.0, offset.dy);
+        inputModel.handlePointerDevicePos(
+            kPointerEventKindMouse, x, y, true, kMouseEventTypeDefault);
+      },
+      onPointerDown: (evt) {
+        onPointerDown?.call(evt);
+      },
+      onPointerUp: (evt) {
+        onPointerUp?.call(evt);
+      },
+      child: MouseRegion(
+        cursor: MouseCursor.defer,
+        onEnter: onEnter,
+        onExit: onExit,
+        child: child,
+      ),
+    );
+  }
+}
--- flutter/lib/common/widgets/remote_input/remote_input.dart
+++ flutter/lib/common/widgets/remote_input/remote_input.dart
@@ -14,6 +14,7 @@ import 'package:flutter_hbb/models/input_model.dart';
 import '.././gestures.dart';
 part 'pans.dart';
 part 'taps.dart';
+part 'pointer_regions.dart';
 
 class RawKeyFocusScope extends StatelessWidget {
   final FocusNode? focusNode;
@@ -211,96 +212,5 @@ class _RawTouchGestureDetectorRegionState
           ..onThreeFingerVerticalDragUpdate = onThreeFingerVerticalDragUpdate;
       }),
     };
-  }
-}
-
-class RawPointerMouseRegion extends StatelessWidget {
-  final InputModel inputModel;
-  final Widget child;
-  final MouseCursor? cursor;
-  final PointerEnterEventListener? onEnter;
-  final PointerExitEventListener? onExit;
-  final PointerDownEventListener? onPointerDown;
-  final PointerUpEventListener? onPointerUp;
-
-  RawPointerMouseRegion({
-    this.onEnter,
-    this.onExit,
-    this.cursor,
-    this.onPointerDown,
-    this.onPointerUp,
-    required this.inputModel,
-    required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Listener(
-      onPointerHover: inputModel.onPointHoverImage,
-      onPointerDown: (evt) {
-        onPointerDown?.call(evt);
-        inputModel.onPointDownImage(evt);
-      },
-      onPointerUp: (evt) {
-        onPointerUp?.call(evt);
-        inputModel.onPointUpImage(evt);
-      },
-      onPointerMove: inputModel.onPointMoveImage,
-      onPointerSignal: inputModel.onPointerSignalImage,
-      onPointerPanZoomStart: inputModel.onPointerPanZoomStart,
-      onPointerPanZoomUpdate: inputModel.onPointerPanZoomUpdate,
-      onPointerPanZoomEnd: inputModel.onPointerPanZoomEnd,
-      child: MouseRegion(
-        cursor: inputModel.isViewOnly
-            ? MouseCursor.defer
-            : (cursor ?? MouseCursor.defer),
-        onEnter: onEnter,
-        onExit: onExit,
-        child: child,
-      ),
-    );
-  }
-}
-
-class CameraRawPointerMouseRegion extends StatelessWidget {
-  final InputModel inputModel;
-  final Widget child;
-  final PointerEnterEventListener? onEnter;
-  final PointerExitEventListener? onExit;
-  final PointerDownEventListener? onPointerDown;
-  final PointerUpEventListener? onPointerUp;
-
-  CameraRawPointerMouseRegion({
-    this.onEnter,
-    this.onExit,
-    this.onPointerDown,
-    this.onPointerUp,
-    required this.inputModel,
-    required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Listener(
-      onPointerHover: (evt) {
-        final offset = evt.position;
-        double x = offset.dx;
-        double y = max(0.0, offset.dy);
-        inputModel.handlePointerDevicePos(
-            kPointerEventKindMouse, x, y, true, kMouseEventTypeDefault);
-      },
-      onPointerDown: (evt) {
-        onPointerDown?.call(evt);
-      },
-      onPointerUp: (evt) {
-        onPointerUp?.call(evt);
-      },
-      child: MouseRegion(
-        cursor: MouseCursor.defer,
-        onEnter: onEnter,
-        onExit: onExit,
-        child: child,
-      ),
-    );
   }
 }
